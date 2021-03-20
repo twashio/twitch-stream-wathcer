13bc36ec3e6c90948e1fad9e61ef025305864b4c
twashio <68735868+twashio@users.noreply.github.com>
2021-03-20 11:08:33 +0900

    Add content to about and news

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var Firestore = require('@google-cloud/firestore');
 
+// setup Firestore
+const db = new Firestore({
+  projectId: 'unkochan-live-info',
+  keyFilename: './key.json',
+});
+
+class articleClass {
+  constructor(header, publishTime) {
+    this.header = header;
+    this.publishTime = publishTime;
+  }
+}
+
+async function getNews() {
+  var news = [];
+  const newsRef = await db.collection('news');
+  const snapshot = await newsRef.orderBy('publishTime', 'desc').get();
+  await snapshot.forEach(article => {
+    const data = article.data();
+    const publishTime = new Date(data.publishTime._seconds).toLocaleString();
+    news.push(new articleClass(data.header, publishTime));
+  });
+  return news;
+}
 // GET news page
 router.get('/', async function (req, res, next) {
-  res.render('news')
+  const news = await getNews();
+  res.render('news', {
+    news: news
+  });
 });
 
 module.exports = router;
