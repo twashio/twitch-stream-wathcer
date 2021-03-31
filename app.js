8beb2ccc8c2e870c435c9fa4f4893cf08057f98c
twashio <68735868+twashio@users.noreply.github.com>
2021-03-31 23:22:14 +0900

    Delete news router

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var sassMiddleware = require('node-sass-middleware');
 var indexRouter = require('./routes/index');
 var pollingRouter = require('./routes/polling');
 var aboutRouter = require('./routes/about');
-var newsRouter = require('./routes/news');
 
 var app = express();
 
@@ -31,7 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/polling', pollingRouter);
 app.use('/about', aboutRouter);
-app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
